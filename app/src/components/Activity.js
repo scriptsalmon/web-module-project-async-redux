35eabcb8a4fc1dc2bd2ef38705bf58eaf6dd7e2b
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2022-07-07 13:05:43 -0700

    feat: participants are displayed as emojis!

--- app/src/components/Activity.js
+++ app/src/components/Activity.js
@@ -2,14 +2,38 @@ import React from "react";
 import { connect } from "react-redux";
 
 import { getActivity } from "./../actions";
+// import Emoji from "./Emoji";
 
-const Activity = ({ activity, isFetching, error, getActivity, handleFavsList }) => {
+const Activity = ({
+  activity,
+  isFetching,
+  error,
+  getActivity,
+  handleFavsList,
+}) => {
+  // const [participants, setParticipants] = useState([]);
+  const participants = [];
+
   if (error) {
     return <h2>We have a problem..!{error}</h2>;
   }
 
   const handleClick = () => {
     getActivity();
+  };
+
+  const createType = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+
+  const createParticipants = (num) => {
+    let peeps = ["🧙‍♀️", "🧙‍♂️", "🧟‍♀️", "🧟", "🧝‍♀️", "🧝", "🦹‍♀️", "🦹‍♂️", "🧛‍♀️", "🧛"];
+    for (let i = 0; i <= num; i++) {
+      let random = Math.floor(Math.random() * peeps.length);
+      participants.push(peeps[random]);
+      // setParticipants([...participants, peeps[random]]);
+    }
+    return participants;
   };
 
   const handleFavorite = (e) => {
@@ -29,7 +53,7 @@ const Activity = ({ activity, isFetching, error, getActivity, handleFavsList }) 
   };
 
   return (
-      <>
+    <>
       {!isFetching && (
         <div className="activityContainer">
           <div className="activityCard" onPointerDown={() => handleClick()}>
@@ -40,12 +64,17 @@ const Activity = ({ activity, isFetching, error, getActivity, handleFavsList }) 
             )}
             {activity.type && (
               <div className="activityItem">
-                <h3>Type</h3> <span>{activity.type}</span>
+                <h3>Type</h3> <span>{createType(activity.type)}</span>
               </div>
             )}
             {activity.participants && (
               <div className="activityItem">
-                <h3>Participants</h3> <span>{activity.participants}</span>
+                <h3>Participants</h3>
+                {/* {participants.map((peep) => {
+                  console.log(peep);
+                  return <Emoji symbol={peep} />;
+                })} */}
+                <span className="emoji">{createParticipants(activity.participants)}</span>
               </div>
             )}
           </div>
@@ -70,6 +99,8 @@ const Activity = ({ activity, isFetching, error, getActivity, handleFavsList }) 
           <div className="activityCard">
             <span>...</span>
           </div>
+          <div className="tab favoriteButton"></div>
+          <div className="tab listButton"></div>
         </div>
       )}
     </>
